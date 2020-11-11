af6fd6e4ffad497e903794e6bc8ea0344f0e2d9e
NaumovKirill62 <k.naumov1996@gmail.com>
2020-11-12 00:00:47 +0300

    канбан меню

--- src/kanban/script.js
+++ src/kanban/script.js
@@ -1,44 +1,21 @@
-let menu = document.getElementById('men');
-let obj = document.getElementById('obj');
+let arrow = document.getElementById('arrow');
 let profile = document.getElementById('profile');
-let div = document.createElement('div');
+let menuProfile = document.createElement('div');
+let parent = document.querySelector('#profile');
 
-div.className = "menuProfile";
-div.id = "menuProfile";
+menuProfile.classList.add("menuProfile");
+menuProfile.id = "menuProfile";
 
-div.innerHTML= "<ul><a href='#'><li>My account</li></a><a href='#'><li>My tasks</li></a><a href='#'><li>Log out</li></a></ul>";
+menuProfile.innerHTML= "<ul><a href='#'><li>My account</li></a><a href='#'><li>My tasks</li></a><a href='#'><li>Log out</li></a></ul>";
 function show() {
-  menu.classList.toggle('rotate');
+  arrow.classList.toggle('rotate');
   let element = document.getElementById("menuProfile");
   if (element !== null) {
-    div.remove();
+    menuProfile.remove();
   } else {
-    document.body.append(div);
+    parent.append(menuProfile);
   }
 }
 
-profile.onmouseenter = function(){
-  profile.style.backgroundColor = "tomato";
-  profile.style.borderRadius = '50%';
-  profile.style.transition = '300ms linear';
-  obj.style.fill = "tomato";
-  obj.style.transition = "300ms linear";
-}
-profile.onmouseleave = function() {
-  profile.style.backgroundColor = "white";
-  obj.style.fill = "white"; 
-}
-menu.onmouseenter = function(){
-  profile.style.backgroundColor = "tomato";
-  profile.style.borderRadius = '50%';
-  profile.style.transition = '300ms linear';
-  obj.style.fill = "tomato";
-  obj.style.transition = "300ms linear";
-}
-menu.onmouseleave = function() {
-  profile.style.backgroundColor = "white";
-  obj.style.fill = "white"; 
-}
-
- menu.onclick = show;
+ arrow.onclick = show;
  profile.onclick = show ;
