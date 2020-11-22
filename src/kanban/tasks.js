092ece337c56e7e2787aa65539012ac4fb4163f8
NaumovKirill62 <k.naumov1996@gmail.com>
2020-11-22 21:20:28 +0300

    Правки.

--- src/kanban/tasks.js
+++ src/kanban/tasks.js
@@ -6,20 +6,20 @@
 function UID() {
   let nodes = ((Date.now()*Math.random())/10000)
     .toString(16)
-    .split('.')
+    .split('.');
   nodes.forEach((item, i, array) => {
     // eslint-disable-next-line no-param-reassign
-    array[i] = item.padEnd(8, '0')
+    array[i] = item.padEnd(8, '0');
   });
-  return nodes.join('-')
-}
+  return nodes.join('-');
+};
 
 /**
  * Список задач
  */
 const TaskList = function(name) {
-  this.title = name
-  this.issues = []
+  this.title = name;
+  this.issues = [];
 }
 
 /**
@@ -27,9 +27,10 @@ const TaskList = function(name) {
  */
  TaskList.createGroup = (...args) => {
    let group = [];
-   for (var arg of args) {
-     group.push(new TaskList(arg));
-   }
+   args.map(function(arg) {
+    group.push(new TaskList(arg));
+    });
+
    return group;
  }
 
@@ -39,78 +40,84 @@ TaskList.prototype = {
    * Поиск индекса по id
    */
   _search(id) {
-    for (var i = 0; i < this.issues.length; i++) {
-      if (this.issues[i].id === id) {
-        return i
-      }
-    }
-    return null
+    const i = this.issues.findIndex((issue) => issue.id === id );
+
+    return i;
   },
 
   /**
    * Получить число задач
    */
   get length() {
-    return this.issues.length
+    return this.issues.length;
   },
 
   /**
    * Получить задачу по id
    */
   get(id) {
-    return this.issues[this._search(id)]
+    return this.issues[this.find(elem => elem.id === id)];
   },
 
   /**
    * Добавить задачу
    */
   add(name) {
-    let id = UID()
-    this.issues.push({ id, name })
-    return id
+    let id = UID();
+
+    this.issues.push({ id, name });
+    
+    return id;
   },
 
   /**
    * Удалить задачу по id
    */
   remove(id) {
-    let index = this._search(id)
-    return index !== null ? this.issues.splice(index, 1)[0] : false
+    let index = this._search(id);
+
+    return index !== null ? this.issues.splice(index, 1)[0] : false ;
   },
 
   /**
    * Переместить задачу в другой лист
    */
   moveTo(id, dist) {
-    return dist.issues.push(this.remove(id))
+    if (this.id === false) {
+      return dist.issues.push(this);
+    }
+    return dist.issues.push(this.remove(id));
   }
 }
 
 
 function linkObjects(group) {
-  let objs = []
-  for (var obj of group) {
-    let newobj = {}
+  let objs = group.map( function(obj) {
+    let newobj = {};
+
     for (var propName in obj) {
-          newobj[propName] = document.querySelector(obj[propName])
-          if (newobj[propName] === null) {
-            throw new Error(`DOM object ${propName} not found
-              Bad query ${obj[propName]}`)
-          }
+      if (newobj[propName] !== null) {
+        newobj[propName] = document.querySelector(obj[propName]);
+      } else {
+        new Error(`DOM object ${propName} not found Bad query ${obj[propName]}`);
+      }
     }
-    objs.push(newobj)
-  }
-  return objs
+
+    return newobj;
+  })
+
+ return objs;
 }
+
 
 /**
  * Конструктор котроллера цепочки списков
  */
 const ChainController = function(chain) {
-  this.model = chain.model
-  this.view = chain.view
-  this._createEvents()
-  this._renderView()
+  this.model = chain.model;
+  this.view = chain.view;
+  this._createEvents();
+  this._renderView();
 }
 /**
  * Прототип контроллера цепочки
@@ -121,7 +128,7 @@ ChainController.prototype = {
    */
   _forChain(cb) {
     for (var i = 0; i < this.model.length; i++) {
-      cb(i, this.model, this.view)
+      cb(i, this.model, this.view);
     }
   },
 
@@ -131,31 +138,36 @@ ChainController.prototype = {
   
   _rToggle(elem, state) {
     // eslint-disable-next-line no-param-reassign
-    elem.style.display = state ? '' : 'none'
+    elem.style.display = state ? '' : 'none';
   },
 
   /**
    * Функция рендеринга, отристовка списка задач
    */
   _rList(obj, list) {
-    let html = ''
-    for (var elem of list) {
-      html += `<div class="menu">${elem.name}</div>\n`
-    }
+    let html = list.reduce((html,elem) => { 
+      // eslint-disable-next-line no-param-reassign
+      html += `<div class="menu">${elem.name}</div>\n`;
+
+    return html
+  }, '');
     // eslint-disable-next-line no-param-reassign
-    obj.innerHTML = html
+    obj.innerHTML = html;
   },
 
   /**
    * Функция рендеринга, отрисовка пунктов выбора тэга select
    */
   _rSelect(obj, list) {
-    let html = '<option selected value="">select task</option>\n'
-    for (var elem of list) {
-      html += `<option value="${elem.id}">${elem.name}</option>\n`
-    }
+    let html = list.reduce((html,elem) => {
+      // eslint-disable-next-line no-param-reassign
+      html += `<option value="${elem.id}">${elem.name}</option>\n`;
+      
+      return html
+    }, '<option selected value="">select task</option>\n')
+
     // eslint-disable-next-line no-param-reassign
-    obj.innerHTML = html
+    obj.innerHTML = html;
   },
 
   /**
@@ -165,20 +177,19 @@ ChainController.prototype = {
   _renderView() {
     this._forChain((i, model, view) => {
       // Отрисовка списка из модели
-      this._rList(view[i].listSection, model[i].issues)
+      this._rList(view[i].listSection, model[i].issues);
 
       // Убрать все поля ввода
-      this._rToggle(view[i].inputElement, false)
+      this._rToggle(view[i].inputElement, false);
 
       // Если в данном списке нет задач, убрать кнопку в следующем списке
       if (model[i].length === 0 && model[i+1]) {
-        this._rToggle(view[i+1].actionButton, false)
+        this._rToggle(view[i+1].actionButton, false);
       } else {
         if (model[i+1]) {
-          this._rToggle(view[i+1].actionButton, true)
+          this._rToggle(view[i+1].actionButton, true);
         }
-      }
-
+      };
     })
   },
 
@@ -190,48 +201,40 @@ ChainController.prototype = {
     this._forChain((i, model, view) => {
       // Add card button
       view[i].actionButton.addEventListener('click', () => {
-        this._rToggle(view[i].inputElement, true)
+        this._rToggle(view[i].inputElement, true);
       })
 
       // потеря фокуса
       view[i].inputElement.addEventListener('blur', () => {
-        this._rToggle(view[i].inputElement, false)
+        this._rToggle(view[i].inputElement, false);
       })
 
       // Изменение данных в полях ввода, если элемент Input
       if (view[i].inputElement.tagName === 'INPUT') {
-        view[i].inputElement.addEventListener('change', () => {
-          model[i].add(view[i].inputElement.value)
+        view[i].inputElement.addEventListener('blur', () => {
+          model[i].add(view[i].inputElement.value);
           // eslint-disable-next-line no-param-reassign
-          view[i].inputElement.value = ""
-          this._renderView()
+          view[i].inputElement.value = "";
+          this._renderView();
         })
       }
 
       // Рендеринг Списка выбора
       if (view[i].inputElement.tagName === 'SELECT') {
         view[i].actionButton.addEventListener('click', () => {
-          this._rSelect(view[i].inputElement, model[i-1].issues)
+          this._rSelect(view[i].inputElement, model[i-1].issues);
         })
       }
 
       // Выбор элемента из списка
       if (view[i].inputElement.tagName === 'SELECT') {
         view[i].inputElement.addEventListener('change', () => {
-          model[i-1].moveTo(view[i].inputElement.value, model[i])
-          this._renderView()
+          model[i-1].moveTo(view[i].inputElement.value, model[i]);
+          this._renderView();
         })
       }
-
     })
   },
-
-  saveState() {
-
-  },
-  loadState() {
-
-  }
 }
 
 
@@ -257,7 +260,7 @@ let queryGroup = [
     inputElement: '.finished > select',
     actionButton: '#footBlockFinished'
   }
-]
+];
 
 
 // Создаем цепочку из модель данных
@@ -266,11 +269,11 @@ let Modelchain = TaskList.createGroup(
   'Ready',
   'InProgress',
   'Finished'
-)
+);
 
 // Создаем контроллер и передаем ему модель с представлением
 let kanban = new ChainController({
   model: Modelchain,
   // Получаем объекты DOM по ссылка и передаем как представление
   view: linkObjects(queryGroup)
-})
+});
